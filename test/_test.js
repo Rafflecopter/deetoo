9a8460cd03cdf74f050332383c9d66074c718ceb
J.R. Patten <jr@juanpatten.com>
2012-08-04 00:21:35 -0600

    Move test files into test/ folder

---
+++ test/_test.js
@@ -0,0 +1,31 @@
+
+var kue = require('kue')
+,   jobs= kue.createQueue()
+
+,   i = 1
+
+kue.app.listen(3000)
+
+jobs.process('shiner', 3, function(job, $done) {
+    console.log('starting ' + job.data.title)
+    setTimeout(function() {
+        console.log('finished ' + job.data.title)
+        $done()
+    }, 1000)
+})
+
+jobs.on('error', function(err) {
+    console.log('EMITTED - ' + err)
+})
+
+//for (var j=0; j<12; j++) {
+    //console.log('creating #' + j)
+    //jobs.create('shiner', {title:'testing ' + j}).save()
+//}
+
+
+setTimeout(function() {
+    console.log('shutting down...')
+    jobs.shutdown(function(){console.log('SHUT DOWN');process.exit(0)})
+}, 2300)
+
