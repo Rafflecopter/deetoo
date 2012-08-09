1f385099f5d6cbc12bd806319d87c665e33983f4
J.R. Patten <jr@juanpatten.com>
2012-08-09 14:35:03 -0600

    Get rid of garbage collection logging

--- config_default.js
+++ config_default.js
@@ -3,13 +3,7 @@ var SECONDS = 1000
 
 module.exports = {
      port_www:  8888
-
-    ,sigterm_shutdown_timeout: 10 * SECONDS
-    
-    ,garbage_collect: {
-        outfile: function(job) {
-            return ['/tmp/deetoo-gcdump-', job.type, 'UTC'].join('')
-        }
-    }
+    ,sigterm_shutdown_timeout: 5 * MINUTES
+    ,garbage_collect: true
 }
 
